3afac163b429b32baebec438faf8c302dc90dcf5
Sneha <snehasharma9205@gmail.com>
2023-07-17 12:39:43 +0530

    created input components

---
+++ src/library/Input/index.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import styles from "./styles.module.scss";
+import { stylesConfig } from "../../utils";
+
+const classNames = stylesConfig(styles);
+
+export const Input = ({ error, errorMessage, className, ...props }) => {
+	return (
+		<div className={classNames("input__container")}>
+			{props.label ? (
+				<label
+					className={classNames("input__label")}
+					htmlFor={props.id}
+				>
+					{props.label}
+				</label>
+			) : null}
+			<input
+				className={
+					classNames(
+						"input",
+						{ "input--error": error },
+						{ "input--disabled": props.disabled }
+					) + ` ${className}`
+				}
+				onInvalid={(e) => {
+					e.currentTarget.setCustomValidity(errorMessage + "");
+				}}
+				onInput={(e) => {
+					e.currentTarget.setCustomValidity("");
+				}}
+				title={error ? errorMessage : ""}
+				{...props}
+			/>
+		</div>
+	);
+};
+
+export const Textarea = ({ error, errorMessage, className, ...props }) => {
+	return (
+		<div className={classNames("input__container")}>
+			{props.label ? (
+				<label
+					className={classNames("input__label")}
+					htmlFor={props.id}
+				>
+					{props.label}
+				</label>
+			) : null}
+			<textarea
+				className={
+					classNames(
+						"input",
+						{ "input--error": error },
+						{ "input--disabled": props.disabled }
+					) + ` ${className}`
+				}
+				onInvalid={(e) => {
+					e.currentTarget.setCustomValidity(errorMessage + "");
+				}}
+				onInput={(e) => {
+					e.currentTarget.setCustomValidity("");
+				}}
+				title={error ? errorMessage : ""}
+				{...props}
+			/>
+		</div>
+	);
+};
+
+export default Input;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,34 @@
-import React from "react";
-import Button from "../../components/Button";
+import React, { useState } from "react";
+import Button from "../../library/Button";
+import Input, { Textarea } from "../../library/Input";
 import styles from "./styles.module.scss";
 import { stylesConfig } from "../../utils";
 
 const classes = stylesConfig(styles, "home");
 
 const Home = () => {
+	const [value, setValue] = useState("");
 	return (
 		<main className={classes("")}>
 			<Button onClick={() => alert("Clicked")}>Click Me</Button>
+			<Input
+				placeholder="Name"
+				required
+				label="Full Name"
+				value={value}
+				onChange={(e) => {
+					setValue(e.target.value);
+				}}
+			/>
+			<Textarea
+				placeholder="Name"
+				required
+				label="Full Name"
+				value={value}
+				onChange={(e) => {
+					setValue(e.target.value);
+				}}
+			/>
 		</main>
 	);
 };
